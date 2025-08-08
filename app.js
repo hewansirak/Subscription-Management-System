a47d4da812f33f277e7483639eaa6a152fdf5b31
hewansirak <hewansg@gmail.com>
2025-08-08 23:21:29 +0300

    Implemented workflow reminder with email

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflow', workflowRouter);
 
 app.use(errorMiddleware);
 app.use(arcjetMiddleware);
