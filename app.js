fcc8164cbc2a2872f7468a510edbd1dbeb7f4dad
hewansirak <hewansg@gmail.com>
2025-08-08 11:48:26 +0300

    Security using Arcjet

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use('/api/v1/users', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.use(errorMiddleware);
-
+app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API!');
